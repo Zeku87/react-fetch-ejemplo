4d5c95ffdb1e19183d59a03b5af5926c8c936e43
J Ezequiel Gallardo <udev87@outlook.com>
2018-05-17 12:46:47 +0200

    Se ha añadido control de errores

--- components/index.js
+++ components/index.js
@@ -7,29 +7,59 @@ class FetchDemo extends React.Component {
     super(props);
 
     this.state = {
-      posts: []
+      posts: [],
+      isLoading: true,
+      hasErrors: false
     };
   }
 
   componentDidMount() {
+    this.setState({isLoading : true})
     axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
       .then(res => {
         const posts = res.data.data.children.map(obj => obj.data);
         this.setState({ posts });
+        this.setState({isLoading: false});
         console.log(JSON.stringify(posts));
-      });
+      }).catch(
+          error => {
+              this.setState({
+                  isLoading:false,
+                  hasErrors:true
+              });
+          }
+      );
+  }
+
+  renderLoading(){
+      return (<div>Cargando...</div>);
+  }
+
+  renderData(){
+    if(this.state.hasErrors === true)
+        return this.renderError();
+    
+    return (
+        <div>            
+            <ul>
+            {this.state.posts.map(post =>
+                <li key={post.id}>{post.title}</li>
+            )}
+            </ul>
+        </div>
+    );
+  }
+
+  renderError(){
+      return( <div>ERROR: revise la url, quizás no tenga derechos de acceso o es incorrecta o bien no tiene conexión a internet</div>);
   }
 
   render() {
-    return (
-      <div>
-        <h1>Ejemplo de llamada asíncrona a una API usando promesas mediante Axios en React</h1>
-        <ul>
-          {this.state.posts.map(post =>
-            <li key={post.id}>{post.title}</li>
-          )}
-        </ul>
-      </div>
+    return(
+        <div>
+            <h1>Ejemplo de llamada asíncrona a una API usando promesas mediante Axios en React</h1>
+            {this.state.isLoading == true ? this.renderLoading() : this.renderData()}
+        </div>
     );
   }
 }
